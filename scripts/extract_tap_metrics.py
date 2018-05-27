29eb6116277cfc287ef674d68c12e678e82db078
kharrigian <Keith@Keiths-MacBook-Air.local>
2018-05-27 19:41:00 -0400

    Beginning analysis

--- scripts/extract_tap_metrics.py
+++ scripts/extract_tap_metrics.py
@@ -91,6 +91,11 @@ for sub, subject_file in enumerate(tapping_filenames):
         metronome_beats = np.nonzero(np.diff(abs(metronome)) == 1)[0] + 1
         expected_iti = np.diff(metronome_beats)[0]
 
+		# Ignore Trial if Not 40 seconds long
+        if len(metronome) != 40 * 2000:
+            print("Subject %s, trial %s is not the full experiemental length" % (subject_id, trial))
+            continue
+
         # Separate Synchronization/Continuation
         met_itis = trial_tap_data['metronome']
         nomet_itis = trial_tap_data['no_metronome']
@@ -98,21 +103,21 @@ for sub, subject_file in enumerate(tapping_filenames):
         # Important Sections
         met_mask_last5 = met_itis[:,1] >= 20000
         met_mask_last10 = met_itis[:,1] >= 10000
-        nomet_mask_last15 = nomet_itis[:,1] >= metronome.shape[0] - (15*2000)
+        nomet_mask_last20 = nomet_itis[:,1] >= 40000
         nomet_mask_nontransient_first5 = np.logical_and(nomet_itis[:,1] >= 40000,  nomet_itis[:,1] < 50000)
         nomet_mask_last5 = nomet_itis[:,1] >= metronome.shape[0] - (5*2000)
 
         # Relative Synchronization Error (median - trial period) / trial_period
         met_last_5_sync_error = (np.median(met_itis[met_mask_last5][:,2]) - expected_iti) / expected_iti * 100
         met_last_10_sync_error = (np.median(met_itis[met_mask_last10][:,2]) - expected_iti) / expected_iti * 100
-        nomet_last_15_sync_error = (np.median(nomet_itis[nomet_mask_last15][:,2]) - expected_iti) / expected_iti * 100
+        nomet_last_20_sync_error = (np.median(nomet_itis[nomet_mask_last20][:,2]) - expected_iti) / expected_iti * 100
         nomet_first_5_sync_error = (np.median(nomet_itis[nomet_mask_nontransient_first5][:,2]) - expected_iti) / expected_iti * 100
         nomet_last_5_sync_error = (np.median(nomet_itis[nomet_mask_last5][:,2]) - expected_iti) / expected_iti * 100
 
         # Coefficient of Variation (std/median)
         met_last_5_cv = np.std(met_itis[met_mask_last5][:,2]) / np.median(met_itis[met_mask_last5][:,2])
         met_last_10_cv = np.std(met_itis[met_mask_last10][:,2]) / np.median(met_itis[met_mask_last10][:,2])
-        nomet_last_15_cv = np.std(nomet_itis[nomet_mask_last15][:,2]) / np.median(nomet_itis[nomet_mask_last15][:,2])
+        nomet_last_20_cv = np.std(nomet_itis[nomet_mask_last20][:,2]) / np.median(nomet_itis[nomet_mask_last20][:,2])
         nomet_first_5_cv = np.std(nomet_itis[nomet_mask_nontransient_first5][:,2]) / np.median(nomet_itis[nomet_mask_nontransient_first5][:,2])
         nomet_last_5_cv = np.std(nomet_itis[nomet_mask_last5][:,2]) / np.median(nomet_itis[nomet_mask_last5][:,2])
 
@@ -134,13 +139,13 @@ for sub, subject_file in enumerate(tapping_filenames):
                         # Synchronization Error
                         "met_sync_error_last5":met_last_5_sync_error,
                         "met_sync_error_last10":met_last_10_sync_error,
-                        "nomet_sync_error_last15":nomet_last_15_sync_error,
+                        "nomet_sync_error_last20":nomet_last_20_sync_error,
                         "nomet_sync_error_first5":nomet_first_5_sync_error,
                         "nomet_sync_error_last5":nomet_last_5_sync_error,
                         # Coefficient of Variation
                         "met_cv_last5":met_last_5_cv,
                         "met_cv_last10":met_last_10_cv,
-                        "nomet_cv_last15":nomet_last_15_cv,
+                        "nomet_cv_last20":nomet_last_20_cv,
                         "nomet_cv_first5":nomet_first_5_cv,
                         "nomet_cv_last5":nomet_last_5_cv,
                         # Drift
@@ -157,6 +162,29 @@ processed_results = pd.DataFrame(processed_results)
 
 # Merge with survey data
 processed_results = pd.merge(processed_results, survey_data, left_on = "subject", right_on = "subject", how = "left")
+
+# Replace Survey Blanks
+processed_results.replace("BLANK", np.nan, inplace = True)
+
+# Standardize Column Names
+processed_results = processed_results.rename(columns = dict((col, col.replace(" ","_")) for col in processed_results.columns))
+
+# Encode Gender
+processed_results["gender"] = processed_results["gender"].map(lambda val: {"M":0,"F":1}[val] if not pd.isnull(val) else None)
+
+# Encode Musical Experience
+processed_results["musical_experience"] = processed_results["musical_experience"].map(lambda val: {"N":0,"Y":1}[val] if not pd.isnull(val) else None)
+processed_results["musical_experience_yrs"] = processed_results["specify_musical_experience"].map(lambda i: max([-1] + [float(j) for j in i.split() if j.isdigit()]) if not pd.isnull(i) else 0)
+
+# Encode Athletic Experience
+processed_results["sport_experience"] = processed_results["sport_experience"].map(lambda val: {"N":0,"Y":1}[val] if not pd.isnull(val) else None)
+processed_results["sport_experience_yrs"] = processed_results["specify_sports_experience"].map(lambda i: max([-1] + [float(j) for j in i.split() if j.isdigit()]) if not pd.isnull(i) else 0)
+
+# Encode Disorder
+processed_results["disorders"] = processed_results["disorders"].map(lambda val: {"N":0,"Y":1}[val] if not pd.isnull(val) else None)
+
+# Encode Trial Speed
+processed_results["trial_speed"] = processed_results["trial_speed"].map(lambda val: {0.8:"SpedUp", 1.0:"NoChange", 1.2:"SlowedDown"}[val] )
 
 # Save
 processed_results.to_csv("./data/processed_results.csv", encoding = "utf-8", index = False)
--- scripts/libraries/helpers.py
+++ scripts/libraries/helpers.py
@@ -3,6 +3,9 @@
 ### Imports
 ########################
 
+from __future__ import print_function, division  # requires python 2.6 or later (2.7 or later preferred)
+import math
+
 # Standard I/O and Data Handling
 import pandas as pd
 import numpy as np
@@ -72,3 +75,70 @@ def load_pickle(filename, how = "rb"):
 def dump_pickle(pyobject, filename, how = "wb"):
     with open(filename, how) as the_pickle_file:
         pickle.dump(pyobject, the_pickle_file, protocol = 2)
+
+########################
+### Composite Standard Deviation
+########################
+
+__all__ = ['iterable', 'avg', 'sample_SD', 'composite_SD']
+
+def iterable(obj):
+    '''True iff obj is iterable: a list, tuple, or string.'''
+    return hasattr(obj, '__contains__')
+
+def avg(samples):
+    if len(samples) >= 1:
+        return sum(samples) / len(samples)
+    return float('nan')
+
+def sample_SD(samples):
+    '''input is an array of samples; result is the standard deviation'''
+    mean = avg(samples)
+    sum_of_squared_deviations = 0;
+    sd = 0
+    if len(samples) >= 2:
+        for datum in samples:
+            sum_of_squared_deviations += ((datum - mean) * (datum - mean));
+        sd = math.sqrt(sum_of_squared_deviations / (len(samples)-1) );
+    return sd
+
+def composite_SD(means, SDs, ncounts):
+    '''Calculate combined standard deviation via ANOVA (ANalysis Of VAriance)
+       See:  http://www.burtonsys.com/climate/composite_standard_deviations.html
+       Inputs are:
+         means, the array of group means
+         SDs, the array of group standard deviations
+         ncounts, number of samples in each group (can be scalar
+                  if all groups have same number of samples)
+       Result is the overall standard deviation.
+    '''
+    G = len(means)  # number of groups
+    if G != len(SDs):
+        raise Exception('inconsistent list lengths')
+    if not iterable(ncounts):
+        ncounts = [ncounts] * G  # convert scalar ncounts to array
+    elif G != len(ncounts):
+        raise Exception('wrong ncounts list length')
+
+    # calculate total number of samples, N, and grand mean, GM
+    N = sum(ncounts)  # total number of samples
+    if N <= 1:
+        raise Exception("Warning: only " + str(N) + " samples, SD is incalculable")
+    GM = 0.0
+    for i in range(G):
+        GM += means[i] * ncounts[i]
+    GM /= N  # grand mean
+
+    # calculate Error Sum of Squares
+    ESS = 0.0
+    for i in range(G):
+        ESS += ((SDs[i])**2) * (ncounts[i] - 1)
+
+    # calculate Total Group Sum of Squares
+    TGSS = 0.0
+    for i in range(G):
+        TGSS += ((means[i]-GM)**2) * ncounts[i]
+
+    # calculate standard deviation as square root of grand variance
+    result = math.sqrt((ESS+TGSS)/(N-1))
+    return result
